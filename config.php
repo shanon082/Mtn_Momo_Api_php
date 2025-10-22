8877c7836535a6887feb5b72297c71a2e652b617
shanon082 <shanonsimon082@gmail.com>
2025-10-22 13:12:51 +0300

    removed senstive keys

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$primary_key = "efc825a16cd54e91b257495f3798fc73";
-$secodary_key = "82ab603816854e039508b274aec3dca4";
+$primary_key = ""; //your primary key from sandbox
+$secodary_key = "";// your secondary key from sandbox
